b55275727fb47d063fd12f5b6912d8ba6d275dae
marie-ganon <61749912+marie-ganon@users.noreply.github.com>
2020-04-17 12:22:01 +0200

    Add files via upload

--- Jeu.py
+++ Jeu.py
@@ -20,23 +20,22 @@ def liste_deplacements_possibles(color, plateau) :
     liste_deplacements = []
     for piece in color :
         for a in range(0,8) :
-            for b in range(0,7) :
+            for b in range(0,8) :
                 if deplacement(piece,a,b) != None :
-                    liste_deplacements.append(a,b)
+                    liste_deplacements.append([a,b])
     return liste_deplacements
 
 def liste_deplacements_piece(piece, plateau):
     liste_deplacements = []
     for a in range(0,8) :
-        for b in range(0,7) :
+        for b in range(0,8) :
             if deplacement(piece,a,b,plateau) != None :
-                liste_deplacements.append(a,b)
+                liste_deplacements.append([a,b])
     return liste_deplacements
 
 def test_echec(plateau):
-    noirs = [blabla]
-    blancs = [blablabla]
-    deplacaments_noirs = liste_deplacements_possibles(noirs, plateau)
+    noirs, blancs = find_color(plateau)
+    deplacements_noirs = liste_deplacements_possibles(noirs, plateau)
     deplacements_blancs = liste_deplacements_possibles(blancs, plateau)
     if RB1[0] in deplacements_noirs :
         print("Le roi blanc est en échec")
@@ -46,20 +45,37 @@ def test_echec(plateau):
         return True, noirs
     return False    
 
+
+def find_color(plateau):
+    blancs, noirs = [], []
+    for i in range(0,8):
+        for j in range(0,8):
+            if plateau[i,j] == 0 :
+                pass
+            else:
+                piece = plateau[i,j]
+                if piece[2] == "noir":
+                    noirs.append(piece)
+                else :
+                    blancs.append(piece)
+    return noirs, blancs
+            
+
 def echec_et_mat(plateau):
     if test_echec(plateau) == False :
         pass
     else :
         test, couleur = test_echec(plateau)
         for piece in couleur :
-            for couple in liste_deplacements_piece(piece, plateau):
-                couple[0] = a
-                couple[1] = b
+            liste = liste_deplacements_piece(piece, plateau)
+            for couple in liste :
+                a = couple[0]
+                b = couple[1]
                 copie_plateau = copy.copy(plateau)
                 deplacement(piece, a, b, copie_plateau)
                 if test_echec(copie_plateau) == False :
                     return False
-        return True
+                return True
                     
 
 def deplacement(p,a,b, plateau) :
@@ -80,14 +96,14 @@ def deplacement(p,a,b, plateau) :
 def game(plateau):
     print("Les blancs commencent")
     morts = []
-    while fin de partie(plateau) == None :
+    while fin_de_partie(plateau) == None :
         move_blanc = input("C'est aux blancs de jouer")
         piece = move_blanc[1]
         if piece[2] != "blanc" :
             print("Vous ne pouvez pas jouer une pièce qui n'est pas de votre couleur")
         deplacement(move_blanc, plateau)
-        if echec_et_mat(morts) == None :
-            return echec_et_mat(morts)
+        if echec_et_mat(plateau) == None :
+            return echec_et_mat(plateau)
         else :
              move_noir = input("C'est aux noirs de jouer")
         piece = move_noir[1]
