c0888c01bdf7962e6b72a349320374f1729c403e
marie-ganon <61749912+marie-ganon@users.noreply.github.com>
2020-05-09 21:18:54 +0200

    Add files via upload

--- Stratégie.py
+++ Stratégie.py
@@ -38,31 +38,72 @@ def evalue_plateau(blancs, noirs, plateau, nb_echecs):
     if color == "noir":
         points["blanc"] += 5
         points["noir"] -= 5 
-    delta = nb_echecs["blanc"] - nb_echecs["noir"] :    # on pénalise la couleur qui a subi le plus d'échecs
+    delta = nb_echecs["blanc"] - nb_echecs["noir"]    # on pénalise la couleur qui a subi le plus d'échecs
     if delta >= 0:
         points["blanc"] -= delta
     else:
         points["noirs"] += delta
     return points
 
-def minmax(plateau, nb_echecs): #ops la couleur de l'ordinateur est noire
+def minmax(color_name, plateau, nb_echecs, difficulte): 
+    if difficulte == False:             # on choisit un jeu facile --> prévision du meilleur coup sans anticipation
+        return meilleur_coup(color_name, plateau, nb_echecs)
+    else :                               # on choisit un jeu difficile --> prévision du meilleur coup sur 3 tours
+         meilleure_piece, meilleur_deplacement = 0, 0
+         noirs, blancs = find_color(plateau)
+         if color_name == "blanc" : 
+             color = blancs
+             other_color_name = "noir"
+             other_color = noirs
+         else :
+             color = noirs
+             other_color_name = "blanc"
+             other_color = blancs
+         max_points_color = evalue_plateau(noirs, blancs, plateau, nb_echecs)[color_name]
+         min_points_other_color = evalue_plateau(noirs, blancs, plateau, nb_echecs)[other_color_name]
+         for piece in color :
+             for couple in liste_deplacements_piece(piece, plateau, morts):
+                 a, b = couple[0], couple[1]
+                 copie_plateau = copy.copy(plateau)
+                 copie_nb_echecs = copy.copy(nb_echecs)
+                 copie_morts = copy.copy(morts)
+                 deplacement(piece, a, b, copie_plateau, copie_morts)
+                 noirs, blancs = find_color(copie_plateau)
+                 for piece_adverse in other_color :
+                     deplacement(piece, couple[0], couple[1], copie_plateau, copie_nb_echecs)
+                     meilleure_piece, meilleur_deplacement = meilleur_coup(color_name, copie_plateau, copie_nb_echecs)
+                     points_color_t2 = evalue_plateau(noirs, blancs, plateau, nb_echecs)[color_name]
+                     points_other_color_t2 = evalue_plateau(noirs, blancs, plateau, nb_echecs)[other_color_name]
+                     if points_color_t2 >= max_points_color and points_other_color_t2 <= min_points_other_color : 
+                         max_points_color = points_color_t2
+                         min_points_other_color = points_other_color_t2
+                         meilleure_piece = piece
+                         meilleur_deplacement = couple
+             return meilleure_piece, meilleur_deplacement
+    
+def meilleur_coup(color_name, plateau, nb_echecs):
     noirs, blancs = find_color(plateau)
-    maximum = evalue_plateau(noirs, blancs, plateau, nb_echecs)["noir"]
-    minimum = evalue_plateau(noirs, blancs, plateau, nb_echecs)["blanc"]
-    coup = []
-    piece_choisie = CV
-    for piece in noirs:
+    meilleure_piece, meilleur_deplacement = 0, 0
+    if color_name == "blanc" : 
+        color = blancs
+        other_color_name = "noir"
+    else :
+        color = noirs
+        other_color_name = "blanc"
+    max_color = evalue_plateau(noirs, blancs, plateau, nb_echecs)[color_name]
+    min_other_color = evalue_plateau(noirs, blancs, plateau, nb_echecs)[other_color_name]
+    for piece in color :
         for couple in liste_deplacements_piece(piece, plateau, morts):
-            a, b = couple[0], couple[1]
-            copie_plateau = copy.copy(plateau)
-            copie_nb_echecs = copy.copy(nb_echecs)
-            copie_morts = copy.copy(morts)
-            deplacement(piece, a, b, copie_plateau, copie_morts)
-            noirs, blancs = find_color(copie_plateau)                     # on actualise la liste des pièces restantes sur le plateau selon le coup possible réalisé
-            if evalue_plateau(noirs, blancs, copy_plateau, copie_nb_echecs)["noir"] >= maximum and evalue_plateau(noirs, blancs, copy_plateau, copie_nb_echecs)["blanc"] <= minimum :
-                piece_choisie = piece
-                coup = [a, b]
-                maximum = evalue_plateau(noirs, blancs, plateau, nb_echecs)["noir"]
-                minimum = evalue_plateau(noirs, blancs, plateau, nb_echecs)["blanc"]
-    return piece_choisie, coup
-
+                a, b = couple[0], couple[1]
+                copie_plateau = copy.copy(plateau)
+                copie_nb_echecs = copy.copy(nb_echecs)
+                copie_morts = copy.copy(morts)
+                deplacement(piece, a, b, copie_plateau, copie_morts)
+                noirs, blancs = find_color(copie_plateau)
+                if evalue_plateau(noirs, blancs, copie_plateau, copie_nb_echecs)[color_name] >= max_color and evalue_plateau(noirs, blancs, copie_plateau, copie_nb_echecs)[other_color_name] <= min_other_color :
+                    max_color = evalue_plateau(noirs, blancs, plateau, nb_echecs)[color_name]
+                    min_other_color = evalue_plateau(noirs, blancs, plateau, nb_echecs)[other_color_name]
+                    meilleure_piece = piece
+                    meilleur_deplacement = couple
+    return meilleure_piece, meilleur_deplacement
+    
